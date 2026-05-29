0222f0b961c782cf868d703855a0a914727af82c
Franckm26 <mercuriofranck9@gmail.com>
2026-05-29 17:24:41 +0800

    Performance: Remove inline CSS/images to drastically improve page load speed

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.app')
 
 @section('styles')
-<style>
-<?php
-$adminCssPath = public_path('css/admin.css');
-if (file_exists($adminCssPath)) {
-    echo file_get_contents($adminCssPath);
-}
-?>
-</style>
+<link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 @endsection
 
 @section('page_title')
